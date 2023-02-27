b8851fe9c47bdf24e34c56bdb2e8df1068168b25
FunmilayoOlugbega <94611789+FunmilayoOlugbega@users.noreply.github.com>
2023-02-27 14:44:00 +0100

    Add files via upload

--- Staple.py
+++ Staple.py
@@ -9,21 +9,26 @@ import SimpleITK as sitk
 import numpy as np
 from matplotlib import pyplot as plt
 
-
+# Load atlas
 seg1 = imageio.v2.imread(r"C:\Users\20192059\Documents\Master\CS in MIA\TrainingData\TrainingData\p125\prostaat.mhd")[40,:,:]
 seg2 = imageio.v2.imread(r"C:\Users\20192059\Documents\Master\CS in MIA\TrainingData\TrainingData\p120\prostaat.mhd")[40,:,:]
 seg3 = imageio.v2.imread(r"C:\Users\20192059\Documents\Master\CS in MIA\TrainingData\TrainingData\p127\prostaat.mhd")[40,:,:]
 
+# Convert to SITK image objects
 seg1_sitk = sitk.GetImageFromArray(seg1.astype(np.int16)) 
 seg2_sitk = sitk.GetImageFromArray(seg2.astype(np.int16))
 seg3_sitk = sitk.GetImageFromArray(seg3.astype(np.int16))
 seg_stack = [seg1_sitk, seg2_sitk, seg3_sitk]
 
+# Run STAPLE  algorithm
 STAPLE_seg_sitk = sitk.STAPLE(seg_stack, 1.0 )
 STAPLE_seg = sitk.GetArrayFromImage(STAPLE_seg_sitk)
-#STAPLE_seg[STAPLE_seg<0.9] = 0    
-#STAPLE_seg[STAPLE_seg>0.9] = 1
 
+# Threshold fused image
+STAPLE_seg[STAPLE_seg<0.9] = 0    
+STAPLE_seg[STAPLE_seg>0.9] = 1
+
+# Display atlas
 fig, ax = plt.subplots(1, 4, figsize=(20, 5))
 ax[0].imshow(seg1, cmap='gray')
 ax[0].set_title('Segmentation 1')
