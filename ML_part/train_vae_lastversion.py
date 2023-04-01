38710dcbeb72d3357aae2550d3f58c1b6378db2c
Noor254390 <94611748+Noor254390@users.noreply.github.com>
2023-04-01 21:27:55 +0200

    Add files via upload

---
+++ ML_part/train_vae_lastversion.py
@@ -0,0 +1,206 @@
+import random
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import make_grid
+from tqdm import tqdm
+from matplotlib.pyplot import imshow, figure
+
+import utils
+import vae
+
+# to ensure reproducible training/validation split
+random.seed(42)
+
+# directorys with data and to store training checkpoints and logs
+DATA_DIR = Path.cwd() / "TrainingData"
+CHECKPOINTS_DIR = Path.cwd() / "vae_model_weights"
+CHECKPOINTS_DIR.mkdir(parents=True, exist_ok=True)
+TENSORBOARD_LOGDIR = "vae_runs"
+
+# training settings and hyperparameters
+NO_VALIDATION_PATIENTS = 2
+IMAGE_SIZE = [64, 64]
+BATCH_SIZE = 32
+N_EPOCHS = 70
+DECAY_LR_AFTER = 50
+LEARNING_RATE = 1e-4
+DISPLAY_FREQ = 10
+
+# dimension of VAE latent space
+Z_DIM = 256
+
+
+# function to reduce the
+def lr_lambda(the_epoch):
+    """Function for scheduling learning rate"""
+    return (
+        1.0
+        if the_epoch < DECAY_LR_AFTER
+        else 1 - float(the_epoch - DECAY_LR_AFTER) / (N_EPOCHS - DECAY_LR_AFTER)
+    )
+
+
+# find patient folders in training directory
+# excluding hidden folders (start with .)
+patients = [
+    path
+    for path in DATA_DIR.glob("*")
+    if not any(part.startswith(".") for part in path.parts)
+]
+random.shuffle(patients)
+
+# split in training/validation after shuffling
+partition = {
+    "train": patients[:-NO_VALIDATION_PATIENTS],
+    "validation": patients[-NO_VALIDATION_PATIENTS:],
+}
+
+# load training data and create DataLoader with batching and shuffling
+dataset = utils.ProstateMRDataset(partition["train"], IMAGE_SIZE)
+dataloader = DataLoader(
+    dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    drop_last=True,
+    pin_memory=True,
+)
+
+# load validation data
+valid_dataset = utils.ProstateMRDataset(partition["validation"], IMAGE_SIZE)
+valid_dataloader = DataLoader(
+    valid_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    drop_last=True,
+    pin_memory=True,
+)
+
+# initialise model, optimiser
+vae_model = vae.VAE() # TODO 
+optimizer = torch.optim.Adam(vae_model.parameters()) # TODO 
+# add a learning rate scheduler based on the lr_lambda function
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9) # TODO lr_lambda(0) #
+
+# training loop
+writer = SummaryWriter(log_dir=TENSORBOARD_LOGDIR)  # tensorboard summary
+for epoch in range(N_EPOCHS):
+    print(epoch)
+    current_train_loss = 0.0
+    current_valid_loss = 0.0
+    
+    # TODO 
+    # training iterations
+    vae_model.train()
+    # TODO 
+    running_loss = 0.0
+    for inputs, labels in tqdm(dataloader, position=0):
+        print("INput: '" ,inputs.shape)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        
+        x_recon, mu, logvar = vae_model(inputs) # forward
+        # Evaluate loss
+        loss = vae.vae_loss(inputs, x_recon, mu, logvar) # get loss
+        
+        # Backward pass
+        loss.backward()# backpropaate loss
+        
+        current_train_loss += loss.item()
+        optimizer.step() # Update parameters (optimize)
+#    lr_lambda(optimizer)
+    
+    #scheduler.step() # Update learning rate
+   
+    # evaluate validation loss
+    with torch.no_grad():
+     vae_model.eval()
+     for inputs, x_real in tqdm(valid_dataloader, position=0): # TODO 
+         x_recon, mu, logvar = vae_model(inputs) # forward pass
+         loss = vae.vae_loss(inputs, x_recon, mu, logvar)
+         current_valid_loss += loss.item()
+
+            
+         vae_model.train()
+    # write to tensorboard log
+    writer.add_scalar("Loss/train", current_train_loss / len(dataloader), epoch)
+    writer.add_scalar(
+        "Loss/validation", current_valid_loss / len(valid_dataloader), epoch
+    )
+    scheduler.step() # step the learning step scheduler
+
+    # save examples of real/fake images
+    if (epoch + 1) % DISPLAY_FREQ == 0:
+        img_grid = make_grid(
+            torch.cat((x_recon[:5], inputs[:5])), nrow=5, padding=12, pad_value=-1
+        )
+        writer.add_image(
+            "Real_fake", np.clip(img_grid[0][np.newaxis], -1, 1) / 2 + 0.5, epoch + 1
+        )
+        
+    # TODO: sample noise 
+    num_preds = 10    
+    # Get random noise to generate 10 images from dimension Z_DIM
+    noise = torch.randn(num_preds, Z_DIM)
+
+# %%    # SAMPLE IMAGES
+with torch.no_grad():        
+    pred = vae_model.generator(noise)    
+
+if (epoch + 1) % DISPLAY_FREQ == 0:
+    img_grid = make_grid(
+        torch.cat((pred[:5], pred[:5])), nrow=5, padding=12, pad_value=-1
+    )
+    writer.add_image(
+        "Real_fake", np.clip(img_grid[0][np.newaxis], -1, 1) / 2 + 0.5, epoch + 1
+    )
+
+# %% run
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+for num in range(num_preds):
+    filename = "Generated_image_"+str(num)+"_"+str(epoch)+".mhd"
+    array = pred[num][0]
+    plt.imshow(array, cmap='gray')
+    plt.show()
+    itk_img = sitk.GetImageFromArray(array)
+  #  itk_img.SetSpacing([0.488281, 0.488281, 1])
+    sitk.WriteImage(itk_img, filename)
+
+torch.save(vae_model.state_dict(), CHECKPOINTS_DIR / "vae_model.pth")
+
+
+
+# %% view generated saved images
+
+itk_image = sitk.ReadImage('Generated_image_1_69.mhd')
+image_array = sitk.GetArrayViewFromImage(itk_image)
+print(image_array)
+# plot the image
+plt.imshow(image_array, cmap='gray')
+plt.show()
+
+itk_image = sitk.ReadImage('Generated_image_4_69.mhd')
+image_array2 = sitk.GetArrayViewFromImage(itk_image)
+iamge_array3 = image_array-image_array2
+# plot the image
+plt.imshow(image_array2, cmap='gray')
+plt.show()
+
+# %% see
+itk_image = sitk.ReadImage('TrainingData\p108\mr_bffe.mhd')
+image_array = sitk.GetArrayViewFromImage(itk_image)
+print(image_array.shape)
+# plot the image
+plt.imshow(image_array[10], cmap='gray')
+plt.show()
+
+
+
+
+
+
+
---
+++ ML_part/vae_lastversion.py
@@ -0,0 +1,293 @@
+import torch
+import torch.nn as nn
+
+
+l1_loss = torch.nn.L1Loss()
+l1_minibatch = torch.nn.L1Loss(size_average=False, reduction='sum')
+mse_loss = torch.nn.MSELoss()
+mse_minibatch = torch.nn.MSELoss(size_average=False, reduction='sum')
+cross_loss = nn.CrossEntropyLoss()
+
+
+class Block(nn.Module):
+    """Basic convolutional building block
+
+    Parameters
+    ----------
+    in_ch : int
+        number of input channels to the block
+    out_ch : int     
+        number of output channels of the block
+    """
+
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+        self.relu =  nn.LeakyReLU() 
+        self.bn1 = nn.BatchNorm2d(out_ch)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_ch) 
+
+    def forward(self, x):
+        """Performs a forward pass of the block
+       
+        x : torch.Tensor
+            the input to the block
+        torch.Tensor
+            the output of the forward pass
+        """
+        # a block consists of two convolutional layers
+        # with ReLU activations
+        # use batch normalisation
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)    
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        return x
+    
+
+class Encoder(nn.Module):
+    """The encoder part of the VAE.
+
+    Parameters
+    ----------
+    spatial_size : list[int]
+        size of the input image, by default [64, 64]
+    z_dim : int
+        dimension of the latent space
+    chs : tuple
+        hold the number of input channels for each encoder block
+    """
+
+    def __init__(self, spatial_size=[64, 64], z_dim=256, chs=(1, 64, 128, 256)):
+        super().__init__()
+        # convolutional blocks
+        self.enc_blocks = nn.ModuleList(
+            [Block(chs[i], chs[i + 1]) for i in range(len(chs) - 1)]# TODO = range 0,1,2,3
+        )
+        
+        # max pooling
+        self.pool = nn.MaxPool2d(2)
+        # height and width of images at lowest resolution level = spatial_size/nr_conv_layers
+        _h, _w = 8,8 
+
+        # flattening
+        self.out = nn.Sequential(nn.Flatten(1), nn.Linear(chs[-1] * _h * _w, 2 * z_dim))
+
+    def forward(self, x):
+        """Performs the forward pass for all blocks in the encoder.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            input image to the encoder
+
+        Returns
+        -------
+        list[torch.Tensor]    
+            a tensor with the means and a tensor with the log variances of the
+            latent distribution
+        """
+
+        for block in self.enc_blocks:
+            x = block(x) 
+            x = self.pool(x) 
+        x = self.out(x)
+    
+        return torch.chunk(x, 2, dim=1)  # 2 chunks, 1 each for mu and logvar
+
+
+class Generator(nn.Module): # decoder
+    """Generator of the GAN
+
+    Parameters
+    ----------
+    z_dim : int 
+        dimension of latent space
+    chs : tuple
+        holds the number of channels for each block
+    h : int, optional
+        height of image at lowest resolution level, by default 8
+    w : int, optional
+        width of image at lowest resolution level, by default 8    
+    """
+
+    def __init__(self, z_dim=256, chs=(256, 128, 64, 32), h=8, w=8):
+
+        super().__init__()
+        self.chs = chs
+        self.h = h  
+        self.w = w  
+        self.z_dim = z_dim  
+       
+        self.proj_z = nn.Linear(
+            self.z_dim, self.chs[0] * self.h * self.w
+        )  # fully connected layer on latent space
+        self.reshape = lambda x: torch.reshape(
+            x, (-1, self.chs[0], self.h, self.w)
+        )  # reshaping
+
+        self.upconvs = nn.ModuleList(
+            [nn.ConvTranspose2d(chs[i], chs[i], 2, 2) for i in range(len(chs) - 1)] # TODO: transposed convolution 
+        )
+
+        self.dec_blocks = nn.ModuleList(
+            [nn.Conv2d(chs[i], chs[i+1], 2, 2) for i in range(len(chs)-1)]           
+        )
+        self.head = nn.Sequential(nn.ConvTranspose2d(chs[-1], 1, 61, 1, 2), nn.Tanh())
+
+    def forward(self, z): # decoder
+        """Performs the forward pass of decoder
+
+        Parameters
+        ----------
+        z : torch.Tensor
+            input to the generator
+        
+        Returns
+        -------
+        x : torch.Tensor
+        
+        """
+        
+        x = self.proj_z(z) # TODO: fully connected layer
+   #     print(x.size())
+        x = self.reshape(x)# TODO: reshape to image dimensions , torch.tensor(self.h, self.w)
+   #     print(x.size())
+        for i in range(len(self.chs) - 1):
+            x = self.upconvs[i](x)
+   #         print(x.size())
+            x = self.dec_blocks[i](x) 
+   #         print(x.size())
+        return self.head(x)
+
+
+class VAE(nn.Module):
+    """A representation of the VAE
+
+    Parameters
+    ----------
+    enc_chs : tuple 
+        holds the number of input channels of each block in the encoder
+    dec_chs : tuple 
+        holds the number of input channels of each block in the decoder
+    """
+    def __init__(
+        self,
+        enc_chs=(1, 64, 128, 256),
+        dec_chs=(256, 128, 64, 32),
+    ):
+        super().__init__()
+        self.encoder = Encoder()
+        self.generator = Generator()
+             
+
+    def forward(self, x):
+        """Performs a forwards pass of the VAE and returns the reconstruction
+        and mean + logvar.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            the input to the encoder
+
+        Returns
+        -------
+        torch.Tensor
+            the reconstruction of the input image
+        float
+            the mean of the latent distribution
+        float
+            the log of the variance of the latent distribution
+        """
+        mu, logvar = self.encoder(x)
+        latent_z = sample_z(mu, logvar)
+        
+        output = self.generator(latent_z)
+ #       output = self.head(recon)
+
+        return output, mu, logvar
+
+
+def get_noise(n_samples, z_dim, device="cpu"):
+    """Creates noise vectors.
+    
+    Given the dimensions (n_samples, z_dim), creates a tensor of that shape filled with 
+    random numbers from the normal distribution.
+
+    Parameters
+    ----------
+    n_samples : int
+        the number of samples to generate
+    z_dim : int
+        the dimension of the noise vector
+    device : str
+        the type of the device, by default "cpu"
+    """
+    return torch.randn(n_samples, z_dim, device=device)
+
+
+def sample_z(mu, logvar):
+    """Samples noise vector from a Gaussian distribution with reparameterization trick.
+
+    Parameters
+    ----------
+    mu : float
+        the mean of the distribution
+    logvar : float
+        the log of the variance of the distribution
+    """
+    eps = torch.randn(mu.size(), device=mu.device).to(mu.dtype)
+    return (logvar / 2).exp() * eps + mu
+
+
+def kld_loss(mu, logvar):
+    """Computes the KLD loss given parameters of the predicted 
+    latent distribution. --> regularization term (prevent memorizing)
+
+    Parameters
+    ----------
+    mu : float
+        the mean of the distribution
+    logvar : float
+        the log of the variance of the distribution
+
+    Returns
+    -------
+    float
+        the kld loss
+
+    """   
+    return -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+
+def vae_loss(inputs, recons, mu, logvar):
+    """Computes the VAE loss, sum of reconstruction and KLD loss --> reconstruction loss
+
+    Parameters
+    ----------
+    inputs : torch.Tensor
+        the input images to the vae
+    recons : torch.Tensor
+        the predicted reconstructions from the vae
+    mu : float
+        the predicted mean of the latent distribution
+    logvar : float
+        the predicted log of the variance of the latent distribution
+
+    Returns
+    -------
+    float
+        sum of reconstruction and KLD loss
+    """
+    print(recons.size())
+    return l1_minibatch(inputs, recons) + kld_loss(mu, logvar)
+  #  return cross_loss(inputs, recons) + kld_loss(mu, logvar) # doesn't work
+  #  return mse_loss(inputs, recons)**2 + kld_loss(mu, logvar)
+  #  return l1_loss(inputs, recons) + kld_loss(mu, logvar)
+  #  return mse_minibatch(inputs, recons) + kld_loss(mu,logvar)
+ 
+ # For losses: look at this
+ #https://pytorch.org/docs/stable/nn.html#loss-functions
